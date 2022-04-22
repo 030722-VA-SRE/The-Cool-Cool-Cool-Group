604f8586e32a44dd09a30698bc76647848cde801
JL47 <jorlopez@csumb.edu>
2022-04-22 11:12:23 -0700

    added custom metrics

--- project2/src/main/java/com/revature/service/NinjaService.java
+++ project2/src/main/java/com/revature/service/NinjaService.java
@@ -16,6 +16,10 @@ import com.revature.modals.Users;
 import com.revature.repositories.NinjaRepository;
 import com.revature.repositories.UserRepository;
 
+import io.micrometer.core.annotation.Timed;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+
 @Service
 public class NinjaService {
 
@@ -23,20 +27,38 @@ public class NinjaService {
 	private NinjaRepository ninjaRepo;
 	private UserRepository userRepo;
 	private Logger log = LoggerFactory.getLogger(NinjaService.class);
+	private MeterRegistry meterRegistry;
+	Counter leafVillageCounter;
+	Counter sandVillageCounter;
 
-	
+	private void initCounters() {
+		
+		//List<Ninja> ninjaVillages = ninjaRepo.findByVillage(null);
+		Ninja n = new Ninja();
+		leafVillageCounter = Counter.builder("ninjas.saved").tag("village", n.getVillage()).description("Number of ninjas").register(meterRegistry);
+		sandVillageCounter = Counter.builder("ninjas.saved").tag("village", "Hidden-Sand-Village").description("Number of ninjas").register(meterRegistry);
+	}
 	@Autowired
-	public NinjaService(NinjaRepository ninjaRepo){
+	public NinjaService(NinjaRepository ninjaRepo, MeterRegistry meterRegistry){
 		super();
 		this.ninjaRepo = ninjaRepo;
+		this.meterRegistry = meterRegistry;
 	}
+	
 	// Gets All Ninjas in Database
+	@Timed(value="ninja.time", description="Time spent retrieving ninjas by village")
 	public List<Ninja> getAllNinjas(){
+		
 		return ninjaRepo.findAll();
 	}
 	// Adds/Creates new Ninja in Database
 	@Transactional
 	public Ninja addNinja(Ninja newNinja) {
+		if("Hidden-Leaf-Village".equals(newNinja.getVillage())) {
+			leafVillageCounter.increment();
+		} else if("Hidden-Sand-Village".equals(newNinja.getVillage())) {
+			sandVillageCounter.increment();
+		}
 		
 		return ninjaRepo.save(newNinja);
 	}
@@ -45,7 +67,7 @@ public class NinjaService {
 		return ninjaRepo.findById(ID).orElseThrow(NinjaNotFoundException::new);
 		//ninjaRepo.
 	}
-
+	@Timed(value="ninja.time", description="Time spent retrieving ninjas by village")
 	public List<Ninja> getNinjasByVillage(@Param("village") String village) throws NinjaNotFoundException{
 		if(village == null) {
 			log.error("Village not found: " + village);
@@ -56,28 +78,32 @@ public class NinjaService {
 		return ninjaRepo.findByVillage(village);
 	
 	}
+	@Timed(value="ninja.time", description="Time spent retrieving ninjas by Jutsu")
 	public List<Ninja> getNinjaByJutsu(@Param("jutsu") String jutsu) {
+		if(jutsu == null) {
+			log.error("Jutus must not be null");
+		}
 		return ninjaRepo.findByJutsuLike(jutsu);
 		
 	}
+	@Timed(value="ninja.time", description="Time spent updating ninja")
 	@Transactional
 	public Ninja updateNinjaVillage(int ID, Ninja ninja) throws NinjaNotFoundException {
 		//int ID = Integer.parseInt(id);
-		Ninja n = ninjaRepo.findById(ID).orElseThrow(NinjaNotFoundException::new);
+		Ninja n = ninjaRepo.findById(ID).orElseThrow(() -> new NinjaNotFoundException("Ninja by that: " + ID + "was not found"));
 		
 		ninja.setId(n.getId());
-		//ninja.setDetails(n.getDetails());
 		
 		return ninjaRepo.save(ninja);
 		
 	}
 	@Transactional
 	public boolean deleteNinjaByID(int ID) throws NinjaNotFoundException {
-		try {
-		ninjaRepo.findById(ID).orElseThrow(NinjaNotFoundException::new);
-		} catch(NinjaNotFoundException ninja) {
-			log.error("Ninja not found with that ID: " + ID);
-		}
+		//try {
+		ninjaRepo.findById(ID).orElseThrow(() -> new NinjaNotFoundException("Ninja by that: " + ID + "was not found"));
+		//} catch(NinjaNotFoundException ninja) {
+			//log.error("Ninja not found with that ID: " + ID);
+		//}
 		ninjaRepo.deleteById(ID);
 		return true;
 	}
